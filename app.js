4966e496c1bf1572f9c0e204c3052c2812a1f91f
Gabriella Sinka Pálinkás <fusewent@gmail.com>
2019-03-17 15:01:01 +0100

    remove, splice, indexOf

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ mySamuraiApp.run(function(){
 
 mySamuraiApp.controller("SamuraiController", ['$scope', function($scope){
 
-    $scope.message = "hey";
+    $scope.removeSamurai = function(samurai) {
+        var removedSamurai = $scope.samurais.indexOf(samurai);
+        $scope.samurais.splice(removedSamurai, 1)
+    }
+
     $scope.samurais = [
         {
             name: "Kaname",
             status: "rhonin",
             rate: 50,
             lastShogun: "Nobunaga",
+            flag: "gold",
             available: true
         },
         {
@@ -25,20 +30,23 @@ mySamuraiApp.controller("SamuraiController", ['$scope', function($scope){
             status: "samurai",
             rate: 10,
             lastShogun: "Nobunaga",
+            flag: "gold",
             available: false
         },
         {
             name: "Rido",
             status: "rhonin",
             rate: 55,
-            lastShogun: "Hideyoshi",
+            lastShogun: "Akechi",
+            flag: "red",
             available: true
         },
         {
             name: "Shiki",
             status: "samurai",
             rate: 45,
-            lastShogun: "Ieyasu",
+            lastShogun: "Hideyoshi",
+            flag: "orange",
             available: true
         },
         {
@@ -46,6 +54,7 @@ mySamuraiApp.controller("SamuraiController", ['$scope', function($scope){
             status: "samurai",
             rate: 40,
             lastShogun: "Nobunaga",
+            flag: "gold",
             available: true
         }
     ];
